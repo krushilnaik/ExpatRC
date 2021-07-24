fe8442d6f4b76aa3de15787a291607605d7951de
Krushil Naik <krushilnaik96@gmail.com>
2021-07-24 09:00:35 -0400

    Add crude styling to Items

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -6,12 +6,24 @@ function Item() {
 	return (
 		<div className='item'>
 			<div className='cost'>
-				<span className='currency'>$</span>
-				<span className='amount'>500</span>
+				<select name='currency' id='currency'>
+					<option value='IDR'>IDR</option>
+					<option value='USD'>USD</option>
+					<option value='JPY'>JPY</option>
+					<option value='PHP'>PHP</option>
+				</select>
+				<input type='number' name='amount' id='amount' className='amount' defaultValue={500} />
 			</div>
-			<span className='name'>milk</span>
-			<span className='frequency'>1</span>
-			<button className='delete'>x</button>
+			<input type='text' className='name' />
+			<input type='text' className='frequency' />
+			<div className='button-group'>
+				<button className='edit'>
+					<i className='fas fa-edit'></i>
+				</button>
+				<button className='delete'>
+					<i className='fas fa-times'></i>
+				</button>
+			</div>
 		</div>
 	);
 }
